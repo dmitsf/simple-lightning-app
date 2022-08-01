ce1f3bd5b379e87b7761324e99d08b07ec5ff763
Dmitry Frolov <dmitsf@gmail.com>
2022-08-01 19:30:15 -0300

    Simple app added

---
+++ app.py
@@ -0,0 +1,25 @@
+import lightning as L
+
+
+class WordComponent(L.LightningWork):
+    def __init__(self, word):
+        super().__init__()
+        self.word = word
+
+    def run(self):
+        print(self.word)
+
+
+class LitApp(L.LightningFlow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.hello = WordComponent("hello")
+        self.world = WordComponent("world")
+
+    def run(self):
+        print("This is a simple Lightning app, make a better app!")
+        self.hello.run()
+        self.world.run()
+
+
+app = L.LightningApp(LitApp())
