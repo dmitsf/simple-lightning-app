2311d2df2c08c357237c215ea93008e354927143
Dmitry Frolov <dmitsf@gmail.com>
2022-12-04 21:41:57 -0500

    Check custom deps e2e

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import lightning as L
 
 class WordComponent(L.LightningWork):
     def __init__(self, word):
-        super().__init__(cloud_compute=L.CloudCompute("gpu-fast-multi"))
+        super().__init__(cloud_compute=L.CloudCompute())
         self.word = word
 
     def run(self):
@@ -14,12 +14,12 @@ class LitApp(L.LightningFlow):
     def __init__(self) -> None:
         super().__init__()
         self.hello = WordComponent("hello")
-        self.world = WordComponent("world")
+        #self.world = WordComponent("world")
 
     def run(self):
         print("This is a simple Lightning app, make a better app!")
         self.hello.run()
-        self.world.run()
+        #self.world.run()
 
 
 app = L.LightningApp(LitApp())
